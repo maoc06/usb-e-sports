af8f520a7f9e15c56a0a79e8c2ef05829524badf
juanvalgreen <gabroval@hotmail.com>
2022-07-24 21:02:32 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProfilesComponent } from './components/profiles/profiles.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreateProfileComponent } from './components/create-profile/create-profile.component';
+import { RegisterComponent } from './components/register/register.component';
+
+const appRoutes:Routes=[
+  {path: '', component: HomeComponent},
+  {path: 'myProfile', component: ProfileComponent},
+  {path: 'profiles', component: ProfilesComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'createProfile', component: CreateProfileComponent},
+  {path: 'register', component: RegisterComponent}
+  
+
+];
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    ProfilesComponent,
+    LoginComponent,
+    CreateProfileComponent,
+    RegisterComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/create-profile/create-profile.component.ts
@@ -0,0 +1,13 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-create-profile',
+  templateUrl: './create-profile.component.html',
+  
+})
+export class CreateProfileComponent {
+
+  
+  
+
+}
